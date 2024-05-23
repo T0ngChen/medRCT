efdb598ce8fc289843a91522251d131cd1982978
T0ngChen <tche929@aucklanduni.ac.nz>
2024-05-23 18:37:02 +1000

    fix summary method when bootstrap == F

--- R/utils.R
+++ R/utils.R
@@ -8,23 +8,27 @@
 #' @method summary medRCT
 #' @export
 summary.medRCT <- function(object, ...){
-  index <- grep("^p", names(object$est))[1]
+  if (object$boostrap == TRUE) {
+    index <- grep("^p", names(object$est))[1]
 
-  out1 = cbind(object$est, object$se, object$cilow, object$ciupp, object$pval)[1:index-1,]
-  colnames(out1) <- c("Estimate", "Std. Error", "CI Lower",
-                     "CI Upper", "p-value")
-  cat("\n")
-  cat("Estimated interventional effect: \n\n")
-  stats::printCoefmat(out1, digits = 3, signif.stars = F)
-  out2 = cbind(object$est, object$se, object$cilow, object$ciupp, object$pval)[index:length(object$est),]
-  colnames(out2) <- c("Estimate", "Std. Error", "CI Lower",
-                      "CI Upper", "p-value")
-  cat("\n")
-  cat("Estimated expected outcome in each trial arm:\n\n")
-  stats::printCoefmat(out2, digits = 3, signif.stars = F)
-  cat("\n")
-  cat("Sample Size:", object$sample.size,"\n")
-  cat("\n")
-  cat("Simulations:", object$mcsim,"\n\n")
+    out1 = cbind(object$est, object$se, object$cilow, object$ciupp, object$pval)[1:index-1,]
+    colnames(out1) <- c("Estimate", "Std. Error", "CI Lower",
+                        "CI Upper", "p-value")
+    cat("\n")
+    cat("Estimated interventional effect: \n\n")
+    stats::printCoefmat(out1, digits = 3, signif.stars = F)
+    out2 = cbind(object$est, object$se, object$cilow, object$ciupp, object$pval)[index:length(object$est),]
+    colnames(out2) <- c("Estimate", "Std. Error", "CI Lower",
+                        "CI Upper", "p-value")
+    cat("\n")
+    cat("Estimated expected outcome in each trial arm:\n\n")
+    stats::printCoefmat(out2, digits = 3, signif.stars = F)
+    cat("\n")
+    cat("Sample Size:", object$sample.size,"\n")
+    cat("\n")
+    cat("Simulations:", object$mcsim,"\n\n")
+  } else {
+    object$est
+  }
 }
 
