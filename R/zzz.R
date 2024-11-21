a30649172337cc5602b263312f2b1b7d627f5b67
T0ngChen <tche929@aucklanduni.ac.nz>
2024-11-22 00:56:20 +1100

    update startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(paste0(
     "medRCT", ": ", utils::packageDescription("medRCT")$Title, "\n",
-    "IMPORTANT: when estimating the effect type 'shift_k_order', the order of mediators specified in the 'mediators' argument is important."
+    "When estimating the effect type 'shift_k_order', the order of mediators specified in the 'mediators' argument is important."
   ))
 }
